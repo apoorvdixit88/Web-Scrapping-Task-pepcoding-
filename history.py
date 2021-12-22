5ba237aa8256cd87bbc159c89538dbb819fdad2d
Apoorv Dixit <64925866+apoorvdixit88@users.noreply.github.com>
2021-12-22 17:45:47 +0530

    code for scrapping required pages

---
+++ history.py
@@ -0,0 +1,29 @@
+import nltk 
+import urllib
+import bs4 as bs
+import re
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+nltk.download('punkt')
+
+source =urllib.request.urlopen('https://en.wikipedia.org/wiki/A#History')
+
+soup =bs.BeautifulSoup(source,'lxml')
+text=""
+for para in soup.find_all('p'):
+    text+=para.text
+
+text=re.sub(r'\[[0-9]*\]',' ',text)
+text=re.sub(r'\s+',' ',text)
+text=text.lower();
+text=re.sub(r'\d',' ',text)
+text=re.sub(r'\s+',' ',text)
+
+sentences=nltk.sent_tokenize(text)
+sentences=[nltk.word_tokenize(sentence) for sentence in sentences]
+pritn(sentences)
+# listToStr = ' '.join([str(elem) for elem in sentences])
+# f=open("histo.txt","w")
+# f.write(listToStr.encode("utf-8"))
+# f.close()
+# # print(sentences)
